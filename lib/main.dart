53c28b46f4ba7dcbd65e6139ef2d7dc7a6df785c
Mayank Rai <mayankrai271993@gmail.com>
2021-04-25 11:40:21 +0200

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_github/app.dart';
@@ -7,12 +10,28 @@ import 'redux/app_store.dart';
 
 void main() => run();
 
+FirebaseAnalytics _firebaseAnalytics;
+
 Future run() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   setup();
   var store = await createStore();
-  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  setUpFirebaseStuff();
+
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
     runApp(App(store: store));
   });
+}
+
+void setUpFirebaseStuff() {
+	_firebaseAnalytics = FirebaseAnalytics();
+	_firebaseAnalytics.logEvent(name: "App Started");
+
+	FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+	FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
 }
